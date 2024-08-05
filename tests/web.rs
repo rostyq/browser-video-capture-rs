a092e50d4b69696e31862dad588aeb670343eece
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2024-08-05 16:54:29 +0300

    test: rewrite tests to use fixtures

--- tests/web.rs
+++ tests/web.rs
@@ -1,19 +1,23 @@
-//! Test suite for the Web and headless browsers.
-
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
 
+#[cfg(test)]
+pub(crate) mod wasm {
+    pub use wasm_bindgen_test::wasm_bindgen_test as test;
+}
+
+use image::Rgba;
+use rstest::*;
+use wasm_bindgen_test::*;
+
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
-use wasm_bindgen_test::*;
 
 use web_sys::{
     js_sys::Promise, wasm_bindgen::JsCast, CanvasRenderingContext2d, HtmlCanvasElement,
     HtmlVideoElement, MediaStream, MediaStreamTrack, OffscreenCanvas,
 };
-
-use image::Rgba;
 
 #[allow(unused_imports)]
 use gloo::{
@@ -22,79 +26,25 @@ use gloo::{
 };
 
 use browser_video_capture::{
-    BrowserVideoCapture, CaptureColor, CaptureMode, HtmlCapture2D, OffscreenCapture2D, OffscreenCaptureGL,
+    impl_canvas_capture_area, BrowserCapture, BrowserCaptureBuilder, BrowserVideoCapture, CaptureArea, CaptureMode, HtmlContextOptions2D, OffscreenContextOptions2D, OffscreenContextOptionsGL, SupportedCanvas, SupportedOptions
 };
 
 const DEFAULT_WIDTH: u32 = 300;
 const DEFAULT_HEIGHT: u32 = 150;
 
-macro_rules! create_canvas {
-    (html $w:expr,$h:expr) => {
-        create_html_canvas($w, $h)
-    };
-    (offscreen $w:expr,$h:expr) => {
-        create_offscreen_canvas($w, $h)
-    };
-}
-
-macro_rules! create_capture {
-    ($cv:tt $kind:ty, $color:expr, ($w:expr,$h:expr)) => {
-        <$kind>::from_canvas(create_canvas!($cv $w,$h), $color)
-            .unwrap()
-            .unwrap()
-            .validate()
-            .unwrap()
-    };
-    [$($cv:tt $kind:ty, $color:expr, ($w:expr,$h:expr));*] => {
-        {
-            let mut arr = Vec::new();
-            $(
-                arr.push(Box::new(create_capture!($cv $kind, $color, ($w,$h))) as Box<dyn BrowserVideoCapture>);
-            )*
-            arr
-        }
-    };
-}
-
-macro_rules! create_video {
-    () => {
-        document()
-            .create_element("video")
-            .unwrap()
-            .dyn_into::<HtmlVideoElement>()
-            .unwrap()
-    };
-    ($width:expr, $height:expr) => {{
-        let v = create_video!();
-        v.set_width($width);
-        v.set_height($height);
-        v.set_muted(true);
-        v.set_autoplay(true);
-        v.toggle_attribute("playsinline").unwrap();
-        v
-    }};
-    (pseudo_hidden) => {{
-        let v = create_video!(1, 1);
-        body().append_child(&v).unwrap();
-        v
-    }};
-}
-
-pub struct CaptureContext {
+pub struct CaptureSetup {
     pub video: HtmlVideoElement,
     pub canvas: HtmlCanvasElement,
     pub context: CanvasRenderingContext2d,
     pub stream: MediaStream,
 }
 
-impl CaptureContext {
-    pub fn new(width: u32, height: u32) -> Self {
-        let canvas = create_html_canvas(width, height);
-        let stream = canvas.capture_stream().unwrap();
-
+impl CaptureSetup {
+    pub fn new(canvas: HtmlCanvasElement, video: HtmlVideoElement) -> Self {
         body().append_child(&canvas).unwrap();
+        body().append_child(&video).unwrap();
 
-        let video = create_video!(pseudo_hidden);
+        let stream = canvas.capture_stream().unwrap();
         video.set_src_object(Some(&stream));
 
         let context = canvas
@@ -111,9 +61,23 @@ impl CaptureContext {
             stream,
         }
     }
+
+    pub fn from_size(source_width: u32, source_height: u32) -> Self {
+        let video = video_source(source_width, source_height);
+        let canvas = document()
+            .create_element("canvas")
+            .unwrap()
+            .dyn_into::<HtmlCanvasElement>()
+            .unwrap();
+        canvas.set_width(source_width);
+        canvas.set_height(source_height);
+        Self::new(canvas, video)
+    }
 }
 
-impl Drop for CaptureContext {
+impl_canvas_capture_area!(CaptureSetup);
+
+impl Drop for CaptureSetup {
     fn drop(&mut self) {
         self.stream
             .get_tracks()
@@ -127,64 +91,84 @@ impl Drop for CaptureContext {
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-fn capture_ignores_empty_video() {
-    let source = create_video!();
-    let caps = create_capture![
-        html HtmlCapture2D, CaptureColor::RGBA, (DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        offscreen OffscreenCapture2D, CaptureColor::RGBA, (DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        offscreen OffscreenCaptureGL, CaptureColor::RGBA, (DEFAULT_WIDTH, DEFAULT_HEIGHT)
-    ];
+fn create_capture(width: u32, height: u32, options: SupportedOptions) -> BrowserCapture {
+    BrowserCaptureBuilder::default()
+        .canvas(capture_canvas(width, height, options))
+        .options(options)
+        .build()
+        .unwrap()
+        .unwrap()
+}
 
-    for cap in caps.into_iter() {
-        for mode in [
-            CaptureMode::put_top_left(),
-            CaptureMode::Fill,
-            CaptureMode::Pinhole,
-        ] {
-            cap.capture(&source, mode);
-            assert_eq!(cap.capture_width(), DEFAULT_WIDTH);
-            assert_eq!(cap.capture_height(), DEFAULT_HEIGHT);
+#[rstest]
+#[wasm::test]
+fn capture_ignores_empty_video(
+    #[values(
+        CaptureSetup::from_size(0, 0),
+        CaptureSetup::from_size(1, 1),
+        CaptureSetup::from_size(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+    )]
+    setup: CaptureSetup,
+    #[values(
+        HtmlContextOptions2D::default().alpha(true).will_read_frequently(true).into(),
+        OffscreenContextOptions2D::default().alpha(true).will_read_frequently(true).into(),
+        OffscreenContextOptionsGL::default().alpha(true).into()
+    )]
+    options: SupportedOptions,
+    #[values(
+        CaptureMode::put_top_left(),
+        CaptureMode::Fill,
+        CaptureMode::Adjust,
+        CaptureMode::Pinhole
+    )]
+    mode: CaptureMode,
+) {
+    let cap = create_capture(DEFAULT_WIDTH, DEFAULT_HEIGHT, options);
+    cap.capture(&setup.video, mode);
+    assert_eq!(cap.capture_width(), DEFAULT_WIDTH);
+    assert_eq!(cap.capture_height(), DEFAULT_HEIGHT);
 
-            let data = cap.data();
-            assert_eq!(data.len(), cap.buffer_size());
-            console_dbg!(&data[0..8]);
-            for value in data.into_iter() {
-                assert_eq!(value, 0);
-            }
-        }
+    let data = cap.data();
+    assert_eq!(data.len(), cap.buffer_size());
+    console_dbg!(&data[0..8]);
+    for value in data.into_iter() {
+        assert_eq!(value, 0);
     }
 }
 
-#[wasm_bindgen_test]
-async fn all_modes_capture_video_frame_1x1() {
-    let ctx = CaptureContext::new(1, 1);
-    let caps = create_capture![
-        html HtmlCapture2D, CaptureColor::RGBA, (1, 1);
-        offscreen OffscreenCapture2D, CaptureColor::RGBA, (1, 1);
-        offscreen OffscreenCaptureGL, CaptureColor::RGBA, (1, 1)
-    ];
+#[rstest]
+#[wasm::test]
+async fn capture_non_empty_video_same_size(
+    #[values(
+        CaptureSetup::from_size(1, 1),
+        CaptureSetup::from_size(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+    )]
+    setup: CaptureSetup,
+    #[values(
+        HtmlContextOptions2D::default().will_read_frequently(true).into(),
+        OffscreenContextOptions2D::default().will_read_frequently(true).into(),
+        OffscreenContextOptionsGL::default().into()
+    )]
+    options: SupportedOptions,
+    #[values(
+        CaptureMode::put_top_left(),
+        CaptureMode::Fill,
+        CaptureMode::Adjust,
+        CaptureMode::Pinhole
+    )]
+    mode: CaptureMode,
+) {
+    let (width, height) = setup.capture_size();
+    let cap = create_capture(width, height, options);
 
-    ctx.context.set_fill_style(&"white".into());
-    ctx.context
-        .fill_rect(0.0, 0.0, DEFAULT_WIDTH as f64, DEFAULT_HEIGHT as f64);
+    setup.context.set_fill_style(&"white".into());
+    setup.context.fill_rect(0.0, 0.0, width as f64, height as f64);
+    wait_next_frame(&setup.video).await;
 
-    wait_next_frame(&ctx.video).await;
-
-    for cap in caps.iter() {
-        for mode in [
-            CaptureMode::put_top_left(),
-            CaptureMode::Fill,
-            CaptureMode::Pinhole,
-            CaptureMode::Adjust,
-        ] {
-            cap.capture(&ctx.video, mode);
-            let data = cap.image().unwrap().into_rgba8();
-            assert_eq!(data.len(), cap.buffer_size());
-            assert_eq!(data.len(), 4);
-            assert_eq!(data.get_pixel(0, 0), &Rgba([255, 255, 255, 255]));
-        }
-    }
+    cap.capture(&setup.video, mode);
+    let data = cap.image().unwrap().into_rgba8();
+    assert_eq!(data.len(), cap.buffer_size());
+    assert_eq!(data.get_pixel(0, 0), &Rgba([255, 255, 255, 255]));
 }
 
 fn animation_frame() -> JsFuture {
@@ -208,15 +192,34 @@ async fn wait_next_frame(video: &HtmlVideoElement) {
     }
 }
 
-fn create_html_canvas(width: u32, height: u32) -> HtmlCanvasElement {
-    let e = document().create_element("canvas").unwrap();
-
-    e.set_attribute("width", &width.to_string()).unwrap();
-    e.set_attribute("height", &height.to_string()).unwrap();
-
-    e.dyn_into().unwrap()
+fn video_source(width: u32, height: u32) -> HtmlVideoElement {
+    let e = document()
+        .create_element("video")
+        .unwrap()
+        .dyn_into::<HtmlVideoElement>()
+        .unwrap();
+    e.set_width(width);
+    e.set_height(height);
+    e.set_muted(true);
+    e.set_autoplay(true);
+    e.toggle_attribute("playsinline").unwrap();
+    e
 }
 
-fn create_offscreen_canvas(width: u32, height: u32) -> OffscreenCanvas {
-    OffscreenCanvas::new(width, height).unwrap()
+fn capture_canvas(width: u32, height: u32, options: SupportedOptions) -> SupportedCanvas {
+    match options {
+        SupportedOptions::Html2D(_) | SupportedOptions::HtmlGL(_) => SupportedCanvas::Html({
+            let e = document()
+                .create_element("canvas")
+                .unwrap()
+                .dyn_into::<HtmlCanvasElement>()
+                .unwrap();
+            e.set_width(width);
+            e.set_height(height);
+            e
+        }),
+        SupportedOptions::Offscreen2D(_) | SupportedOptions::OffscreenGL(_) => {
+            SupportedCanvas::Offscreen(OffscreenCanvas::new(width, height).unwrap())
+        }
+    }
 }
